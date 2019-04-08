6192406510115bc2d64e29f640a5e02f216b99ab
冷秋 <516563564@qq.com>
2019-04-08 20:49:20 +0800

    Add InstallOpenDev

---
+++ InstallOpenDev/README.md
@@ -0,0 +1,32 @@
+# iOS OpenDev
+
+## 1. Install macports (VPN)
+
+Install [macports](https://www.macports.org/install.php)
+
+Update MacPorts
+
+```shell
+$ sudo port -v selfupdate
+```
+
+## 2. Install dpkg
+
+```shell
+$ sudo port -f install dpkg
+```
+
+## 3. Run shell
+
+```shell
+$ cd "this directory"
+$ sudo /bin/bash pre_install.sh
+```
+
+## 4. Install iOSOpenDev
+
+Download [iOSOpenDev](http://iosopendev.com/download/).
+
+Install iOSOpenDev.
+
+Done.
---
+++ InstallOpenDev/pre_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+iOSSpec="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Specifications"
+cp -r "Specifications/iPhoneOS" "${iOSSpec}"
+
+iSimSpec="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Library/Xcode/Specifications"
+cp -r "Specifications/iPhone Simulator" "${iSimSpec}"
+
+iOSSDK="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library"
+mkdir "${iOSSDK}/PrivateFrameworks"
+
+iSimSDK="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library"
+mkdir "${iSimSDK}/PrivateFrameworks"
+
+mkdir "/Applications/Xcode/Content/Developer/Platforms/iPhoneSimulator.platform/Developer/usr"
+mkdir "/Applications/Xcode/Content/Developer/Platforms/iPhoneSimulator.platform/Developer/usr/bin"
